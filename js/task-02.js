30c939347904be4bcfc79593f43326b64fb7a406
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-20 00:33:35 +0300

    tast 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const result = ingredients.forEach((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = element;
+  itemEl.classList.add("item");
+  console.log(itemEl);
+});
