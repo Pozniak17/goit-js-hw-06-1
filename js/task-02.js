be298b8a2b546c200c33fcdc7877aed8106d74cd
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-20 14:38:59 +0300

    tast 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 
+const listEl = document.querySelector("#ingredients");
+
 const result = ingredients.forEach((element) => {
   const itemEl = document.createElement("li");
   itemEl.textContent = element;
   itemEl.classList.add("item");
-  console.log(itemEl);
+  listEl.append(itemEl);
 });
+
+console.log(listEl);
