8515965270abd5709607416b98ffd14d8cd73b7a
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-21 16:38:40 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,29 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const listEl = document.querySelector(".gallery");
+
+const result = images
+  .map(
+    (item) => `<li><img src="${item.url}" width= 300 alt="${item.url}"></li>`
+  )
+  .join("");
+
+console.log(result);
+listEl.style.display = "flex";
+listEl.style.justifyContent = "space-around";
+
+listEl.style.listStyle = "none";
+listEl.insertAdjacentHTML("beforeend", result);
