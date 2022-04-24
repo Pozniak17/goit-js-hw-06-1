66e78e058e084baef0b1b2638aeee372384e9b3b
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-24 22:45:06 +0300

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+//* 1. ств. лічильник, значення якого = 0
+
+//* 2. ств. слухачів кнопок, які збільшують/зменшують на 1
+
+//* 3. оновити інтерфейс новим значенням counterValue
+
+//! Доступ до кнопки віднімання
+const btnDecrement = document.querySelector('button[data-action="decrement"]');
+
+//! Доступ до кнопки додавання
+const btnIncrement = document.querySelector('button[data-action="increment"]');
+
+//! Доступ до ел. span
+const numberEl = document.querySelector("#value");
+
+let counterValue = 0;
+
+//* кнопка -1
+const listDecr = btnDecrement.addEventListener("click", function () {
+  numberEl.textContent = counterValue -= 1;
+});
+
+//* кнопка +1
+const listIncr = btnIncrement.addEventListener("click", function () {
+  numberEl.textContent = counterValue += 1;
+});
