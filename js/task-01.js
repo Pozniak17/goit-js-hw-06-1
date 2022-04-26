1738c528ed01fdbd37c08e5dac1ba511c32a6d77
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-26 09:49:26 +0300

    task 1 improved

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,14 @@
+//*✅ Number of categories: 3
+
+//*✅ Category: Animals
+//*✅ Elements: 4
+
+//*✅ Category: Products
+//*✅ Elements: 3
+
+//*✅ Category: Technologies
+//*✅ Elements: 5
+
 // ========================== Number of categories ===============================
 const numberOfCategories = document.querySelectorAll("h2").length;
 console.log("Number of categories:", numberOfCategories); //numberOfCategories: 3
@@ -26,41 +37,3 @@ console.log("Category:", technologiesTitle); //Category: Products;
 
 const numberOfTechnologies = document.querySelectorAll(".item > ul")[2];
 console.log("Elements: ", numberOfTechnologies.children.length); //5
-
-//*✅ Number of categories: 3
-
-//*✅ Category: Animals
-//*✅ Elements: 4
-
-//*✅ Category: Products
-//*✅ Elements: 3
-
-//*✅ Category: Technologies
-//*✅ Elements: 5
-
-// const numberOfCategories = document.querySelectorAll("h2").length;
-// console.log("Number of categories:", numberOfCategories); //3
-
-// const animalTitle =
-//   document.querySelector(".item").firstElementChild.textContent;
-// console.log("Category:", animalTitle); //Animals
-
-// const animalsEl =
-//   document.querySelector(".item").lastElementChild.children.length;
-// console.log("Elements:", animalsEl); //4
-
-// const productsTitle =
-//   document.querySelector(".item").nextElementSibling.firstElementChild
-//     .textContent;
-// console.log("Category:", productsTitle); //Products
-
-// const productsEl =
-//   document.querySelectorAll(".item")[1].lastElementChild.children.length;
-// console.log("Elements:", productsEl); //3
-
-// const technologiesTitle =
-//   document.querySelectorAll(".item > h2")[2].textContent;
-// console.log("Category:", technologiesTitle); //Category: Products;
-
-// const numberOfTechnologies = document.querySelectorAll(".item > ul")[2];
-// console.log("Elements: ", numberOfTechnologies.children.length);
