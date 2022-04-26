65bbf63da5deeefbec299cf0367f89d522c20073
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-26 09:48:11 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector("button");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnEl.addEventListener("click", onClickButton);
+
+function onClickButton() {
+  bodyEl.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+
+  spanEl.textContent = bodyEl.style.backgroundColor;
+
+  console.log(spanEl.textContent);
 }
