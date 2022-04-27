22b159eb29fce246b89112ba5c35d08f047f6fdd
Yevhenii Pozniak <jekamanu@gmail.com>
2022-04-27 22:14:58 +0300

    fixed task 1

--- js/task-01.js
+++ js/task-01.js
@@ -9,31 +9,43 @@
 //*✅ Category: Technologies
 //*✅ Elements: 5
 
-// ========================== Number of categories ===============================
-const numberOfCategories = document.querySelectorAll("h2").length;
-console.log("Number of categories:", numberOfCategories); //numberOfCategories: 3
+const listEl = document.querySelectorAll(".item");
 
-// ==================================== Animals ==================================
+console.log(`Number of categories: ${listEl.length}`);
 
-const animalsTitle = document.querySelector("h2").textContent;
-console.log("Category:", animalsTitle); //Category: Animals
+listEl.forEach((item) => {
+  const categoryTitleEl = item.firstElementChild;
+  console.log("Category:", categoryTitleEl.textContent);
 
-const numbersOfAnimals = document.querySelector(".item > ul");
-console.log("Elements:", numbersOfAnimals.children.length); //Elements: 4;
+  const numbersOfEl = item.querySelectorAll("li");
+  console.log("Elements:", numbersOfEl.length);
+});
 
-// ==================================== Products ==================================
+// // ========================== Number of categories ===============================
+// const numberOfCategories = document.querySelectorAll("h2").length;
+// console.log("Number of categories:", numberOfCategories); //numberOfCategories: 3
 
-const productsTitle = document.querySelectorAll(".item > h2")[1].textContent;
-console.log("Category:", productsTitle); //Category: Products
+// // ==================================== Animals ==================================
 
-const numbersOfProducts = document.querySelectorAll(".item > ul")[1];
-console.log("Elements:", numbersOfProducts.children.length);
+// const animalsTitle = document.querySelector("h2").textContent;
+// console.log("Category:", animalsTitle); //Category: Animals
 
-// ==================================== Technologies ===============================
+// const numbersOfAnimals = document.querySelector(".item > ul");
+// console.log("Elements:", numbersOfAnimals.children.length); //Elements: 4;
 
-const technologiesTitle =
-  document.querySelectorAll(".item > h2")[2].textContent;
-console.log("Category:", technologiesTitle); //Category: Products;
+// // ==================================== Products ==================================
 
-const numberOfTechnologies = document.querySelectorAll(".item > ul")[2];
-console.log("Elements: ", numberOfTechnologies.children.length); //5
+// const productsTitle = document.querySelectorAll(".item > h2")[1].textContent;
+// console.log("Category:", productsTitle); //Category: Products
+
+// const numbersOfProducts = document.querySelectorAll(".item > ul")[1];
+// console.log("Elements:", numbersOfProducts.children.length);
+
+// // ==================================== Technologies ===============================
+
+// const technologiesTitle =
+//   document.querySelectorAll(".item > h2")[2].textContent;
+// console.log("Category:", technologiesTitle); //Category: Products;
+
+// const numberOfTechnologies = document.querySelectorAll(".item > ul")[2];
+// console.log("Elements: ", numberOfTechnologies.children.length); //5
